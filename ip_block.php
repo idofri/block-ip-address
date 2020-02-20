07619928a7608b2fbf77efd70be873bfb566c907
ido <ido@imark.co.il>
2020-02-20 16:38:33 +0200

    compatible with cf7

--- ip_block.php
+++ ip_block.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Plugin Name: Block IP Address
+ * Plugin Name: Block IP Address (for cf7)
  * Version:     1.0.0
  * Author:      Ido Friedlander
  * Author URI:  https://github.com/idofri
@@ -9,17 +9,6 @@
 
 class BlockIpAddress
 {
-    private $pool = [
-        '7.7.7.7',
-        '8.8.8.8',
-    ];
-
-    private $ipAddress;
-
-    private $visitTransient = '';
-
-    private $blockTransient = '';
-
     public static function instance()
     {
         static $instance;
@@ -28,100 +17,64 @@ class BlockIpAddress
 
     protected function __construct()
     {
-        $this->setIpAddress()
-            ->setVisitTransient()
-            ->setBlockTransient();
-
-        add_action('wp', [$this, 'init']);
+        add_action('wpcf7_before_send_mail', [$this, 'cf7AfterSubmission'], 10, 3);
+        add_filter('wpcf7_submission_is_blacklisted', [$this, 'cf7BeforeSubmission'], 10, 2);
     }
 
-    public function setIpAddress()
+    public function cf7BeforeSubmission($blacklisted, $submission)
     {
-        $this->ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
-        return $this;
-    }
-
-    public function getIpAddress(): string
-    {
-        return $this->ipAddress;
-    }
-
-    public function setVisitTransient()
-    {
-        $this->visitTransient = 'visit_' . $this->getIpAddress();
-        return $this;
-    }
-
-    public function getVisitTransient(): string
-    {
-        return $this->visitTransient;
-    }
-
-    public function setBlockTransient()
-    {
-        $this->blockTransient = 'block_' . $this->getIpAddress();
-        return $this;
-    }
-
-    public function getBlockTransient(): string
-    {
-        return $this->blockTransient;
-    }
-
-    public function shouldBlockThisPage(): bool
-    {
-        return (strpos($_SERVER['REQUEST_URI'] ?? '', 'thankyou') !== false);
-    }
-
-    public function shouldBlockThisIp(): bool
-    {
-        return (bool) get_transient($this->getBlockTransient());
-    }
-
-    public function ipAddressExcluded(): bool
-    {
-        return in_array($this->getIpAddress(), $this->pool);
-    }
-
-    public function init()
-    {
-        if (!$this->shouldBlockThisPage() || $this->ipAddressExcluded()) {
-            return;
+        $ipAddress = $submission->get_meta('remote_ip');
+        if (!$this->ipAddressExcluded($ipAddress, $submission->get_contact_form())) {
+            return $this->shouldBlockThisIp($ipAddress);
         }
-
-        if ($this->shouldBlockThisIp()) {
-            return $this->blockIpAddress();
-        }
-
-        $visits = $this->getVisits();
-        if ($visits >= 1) {
-            set_transient($this->getBlockTransient(), true, $this->getBlockExpiration());
-        }
-
-        set_transient($this->getVisitTransient(), ++$visits, $this->getVisitRateExpiration());
+        return $blacklisted;
     }
 
-    public function blockIpAddress(): void
+    public function cf7AfterSubmission(WPCF7_ContactForm $form, $abort, WPCF7_Submission $submission)
     {
-        global $wp_query;
-        status_header(401);
-        $wp_query->set_404();
+        $ipAddress = $submission->get_meta('remote_ip');
+        $submits = $this->getSubmits($ipAddress);
+        if ($submits >= 1) {
+            set_transient($this->getBlockTransient($ipAddress), true, $this->getBlockExpiration());
+        }
+        set_transient($this->getSubmitTransient($ipAddress), ++$submits, $this->getSubmitRateExpiration());
     }
 
-    public function getBlockExpiration()
+    public function getSubmitTransient(string $ipAddress): string
+    {
+        return 'submit_' . $ipAddress;
+    }
+
+    public function getBlockTransient(string $ipAddress): string
+    {
+        return 'block_' . $ipAddress;
+    }
+
+    public function shouldBlockThisIp(string $ipAddress): bool
+    {
+        return (bool) get_transient($this->getBlockTransient($ipAddress));
+    }
+
+    public function ipAddressExcluded(string $ipAddress, WPCF7_ContactForm $form): bool
+    {
+        $excludedIps = array_map('trim', explode(',', $form->additional_setting('excluded_ips')[0] ?? ''));
+        return in_array($ipAddress, $excludedIps);
+    }
+
+    public function getBlockExpiration(): int
     {
         return DAY_IN_SECONDS;
     }
 
-    public function getVisitRateExpiration()
+    public function getSubmitRateExpiration(): int
     {
         return MINUTE_IN_SECONDS * 30;
     }
 
-    public function getVisits(): int
+    public function getSubmits(string $ipAddress): int
     {
-        $visits = get_transient($this->getVisitTransient());
-        return $visits ? (int) $visits : 0;
+        $submits = get_transient($this->getSubmitTransient($ipAddress));
+        return $submits ? (int) $submits : 0;
     }
 }
 
